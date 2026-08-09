38974184b5ee6b08e23bf0ec4d14ecddfe9dd471
imwahidshaikh <wahidyshaikh@gmail.com>
2026-08-09 17:16:59 +0530

    config: update backend bucket name for my account

--- envs/qa/backend.tf
+++ envs/qa/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-dpp-2026"
+    bucket       = "zen-pharma-terraform-state-imwahidshaikh"
     key          = "envs/qa/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
