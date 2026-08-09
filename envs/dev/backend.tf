a4cc74fb30e72883b70f5680bb094d1134384cdb
imwahidshaikh <wahidyshaikh@gmail.com>
2026-08-09 18:33:55 +0530

    feat: initial dev environment setup2

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket       = "zen-pharma-terraform-state-dpp-2026"
+    bucket       = "zen-pharma-terraform-state-imwahidshaikh"
     key          = "envs/dev/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
